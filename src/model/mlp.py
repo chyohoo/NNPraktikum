24c083875a2f398095b62c173fbc0df606f8c7c7
YueNing <n1085633848@outlook.com>
2018-06-25 13:55:57 +0000

    realize th e _update_weights function

--- src/model/mlp.py
+++ src/model/mlp.py
@@ -141,7 +141,22 @@ class MultilayerPerceptron(Classifier):
         """
         Update the weights of the layers by propagating back the error
         """
-        pass
+        # @Author : Yue Ning
+        output_layer = self._get_output_layer()
+        input_layer = self._get_input_layer()
+        output_layer.input_data = input_layer.output_data
+        # in order to realize this update_weights function, need deltas
+        # to get deltas in train function 
+        for neuron in range(0, output_layer.nOut):
+            output_layer.weights[:,neuron] -= (learningRate * 
+                                                output_layer.deltas[neuron] * 
+                                                output_layer.input_data)
+            self.layers[-1].weights = output_layer.weights
+        for neuron in range(0, input_layer.nOut):
+            input_layer.weights[:,neuron] -= (learningRate * 
+                                                input_layer.deltas[neuron] * 
+                                                input_layer.input_data)
+            self.layers[0].weights = input_layer.weights
 
     def train(self, verbose=True):
         """Train the Multi-layer Perceptrons
